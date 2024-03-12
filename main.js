dd1c95563e2dabc016e8f67b4e71444835c6ef49
Muni <106088010+muni0112@users.noreply.github.com>
2024-03-12 08:47:39 -0300

    gg

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+
+for ( let i = 0 <botoes.length; j++){
+    botoes[i].onclick = function(){
+        for (let j= 0; < botoes.length; jj++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo")
+    }
+}
